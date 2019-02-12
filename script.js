70658cf4c2e7374fc766abc9902a41cffbd1d1fd
Andy Brown <andrewtcb@gmail.com>
2019-02-12 11:55:43 +1100

    Added create collision detection and diagonal movement, also ball counter

--- script.js
+++ script.js
@@ -4,90 +4,119 @@ window.onload = () => {
 
     class Ball {
         constructor() {
-            this.el = document.createElement("DIV");
+            this.el = document.createElement(`DIV`);
             this.el.style.top = `${Math.random() * winHeight}px`;
             this.el.style.left = `${Math.random() * winWidth}px`;
-            this.el.style.transition = "left 0.05s, top 0.05s";
-            this.dirY = 'down';
-            this.dirX = 'right';
+            this.el.style.transition = `left 0.05s, top 0.05s`;
+            this.dirY = Math.random() < 0.5 ? `down` : `up`;
+            this.dirX = Math.random() < 0.5 ? `right` : `left`;
+
+            const self = this;
+            this.ballsMove = undefined;
+            this.startInterval = () => {
+                self.ballsMove = setInterval(self.start, 50);
+            }
 
             this.start = () => {
-                setInterval(() => {
-                    const evilBallPosY = parseInt(this.el.style.top);
-                    const evilBallPosX = parseInt(this.el.style.left);
+                const evilBallPosY = parseInt(this.el.style.top);
+                const evilBallPosX = parseInt(this.el.style.left);
 
-                    // Move the evil ball up or down 
-                    this.el.style.top = this.el.dirY === "down" ? `${evilBallPosY + 10}px` : `${evilBallPosY - 10}px`;
+                // Move the evil ball up or down 
+                this.el.style.top = this.dirY === `down` ? `${evilBallPosY + 10}px` : `${evilBallPosY - 10}px`;
 
-                    // Change direction of evil ball if it hits the wall
-                    if (evilBallPosY > winHeight) {
-                        this.el.dirY = "up";
-                    } else if (evilBallPosY < 10) {
-                        this.el.dirY = "down";
-                    }
+                // Change direction of evil ball if it hits the wall
+                if (evilBallPosY > winHeight) {
+                    this.dirY = `up`;
+                } else if (evilBallPosY < 10) {
+                    this.dirY = `down`;
+                }
 
-                    // Move the ball left or right
-                    this.el.style.left = this.el.dirX === "right" ? `${evilBallPosX + 10}px` : `${evilBallPosX - 10}px`;
+                // Move the ball left or right
+                this.el.style.left = this.dirX === `right` ? `${evilBallPosX + 10}px` : `${evilBallPosX - 10}px`;
 
-                    // Change direction of evil ball if it hits the wall
-                    if (evilBallPosX > winWidth) {
-                        this.el.dirX = "left";
-                    } else if (evilBallPosX < 10) {
-                        this.el.dirX = "right";
-                    }
-                }, 50)
+                // Change direction of evil ball if it hits the wall
+                if (evilBallPosX > winWidth) {
+                    this.dirX = `left`;
+                } else if (evilBallPosX < 10) {
+                    this.dirX = `right`;
+                }
+
+                // Check for collision
+                
+                if (evilBallPosX + 20 >= parseInt(ball1.el.style.left) && evilBallPosX + 20 <= parseInt(ball1.el.style.left) + 20 && evilBallPosY + 20 >= parseInt(ball1.el.style.top) && evilBallPosY + 20 <= parseInt(ball1.el.style.top) + 20
+                    || parseInt(ball1.el.style.left) + 20 >= evilBallPosX && parseInt(ball1.el.style.left) + 20 <= evilBallPosX + 20 && parseInt(ball1.el.style.top) + 20 >= evilBallPosY && parseInt(ball1.el.style.top) + 20 <= evilBallPosY + 20) {
+                    stopBalls()
+                    console.log('collision')
+                }
+            }
+
+            this.stop = () => {
+                clearInterval(self.ballsMove);
             }
         }
     }
 
     let ball1 = new Ball;
-    ball1.el.className = "gameBall";
+    ball1.el.className = `gameBall`;
     document.body.append(ball1.el);
 
-    const evilBalls = ["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10"];
-
-    evilBalls.forEach((b) => {
-        b = new Ball;
-        b.el.className = "evilBall";
-        document.body.append(b.el);
-        b.start();
-    })
+    let ballsList = [];
+    let newBalls;
+    let ballCounter = 0;
     
-    let map = {}; // You could also use an array
+    const createBalls = () => {
+        newBalls = setInterval( function() {
+            let b = new Ball;
+            b.el.className = `evilBall`;
+            document.body.append(b.el);
+            ballsList.push(b)
+            b.startInterval();
+            ballCounter++;
+            document.querySelector('#counter').innerHTML = ballCounter;
+        }, 1000)
+    }
 
-    // window.addEventListener('keydown', (e) => {
-    onkeydown = onkeyup = function (e) {
-        e = e || event; // to deal with IE
-        map[e.keyCode] = e.type == 'keydown';
+    createBalls();
+
+    function stopBalls() {
+        ballsList.forEach( (ball) => {
+            ball.stop();
+            clearInterval(newBalls);
+            controlsOn = false;
+        })
+    }
+    
+    let map = {};
+    let controlsOn = true;
+    onkeydown = onkeyup = (e) => {
+        if (!controlsOn) return;
+        map[e.keyCode] = e.type == `keydown`;
         if (e.keyCode !== 37 && e.keyCode !== 38 && e.keyCode !== 39 && e.keyCode !== 40) return;
-        console.log(e.keyCode)
-        const currentKey = e.keyCode;
 
-        const gameBall = document.querySelector('.gameBall').style;
+        const gameBall = document.querySelector(`.gameBall`).style;
         const { left: posLeft, top: posTop } = gameBall;
         
-        if (map["37"] && map["38"]) {
+        if (map[`37`] && map[`38`]) {
             gameBall.left = `${parseInt(posLeft) - 10}px`;
             gameBall.top = `${parseInt(posTop) - 10}px`;
-        } else if (map["38"] && map["39"]) {
+        } else if (map[`38`] && map[`39`]) {
             gameBall.top = `${parseInt(posTop) - 10}px`;
             gameBall.left = `${parseInt(posLeft) + 10}px`;
-        } else if (map["39"] && map["40"]) {
+        } else if (map[`39`] && map[`40`]) {
             gameBall.left = `${parseInt(posLeft) + 10}px`;
             gameBall.top = `${parseInt(posTop) + 10}px`;
-        } else if (map["40"] && map["37"]) {
+        } else if (map[`40`] && map[`37`]) {
             gameBall.top = `${parseInt(posTop) + 10}px`;
             gameBall.left = `${parseInt(posLeft) - 10}px`;
-        } else if (map["37"]) {
+        } else if (map[`37`]) {
             gameBall.left = `${parseInt(posLeft) - 10}px`;
-        } else if (map["38"]) {
+        } else if (map[`38`]) {
             gameBall.top = `${parseInt(posTop) - 10}px`;
-        } else if (map["39"]) {
+        } else if (map[`39`]) {
             gameBall.left = `${parseInt(posLeft) + 10}px`;
-        } else if (map["40"]) {
+        } else if (map[`40`]) {
             gameBall.top = `${parseInt(posTop) + 10}px`;
         }
-        console.log(map);
     }
     
 }
